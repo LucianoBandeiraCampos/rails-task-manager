8cd2ef7d5a6d2626da8d89dd75c0b6fdb2ba68c5
Luciano Campos <lucianobandeiracampos@gmail.com>
2020-11-27 12:00:21 -0300

    tasks manager fim

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
   # EDIT and UPDATE
-  get '/tasks/:id/edit', to: 'tasks#edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   # DELETE
   delete 'tasks/:id', to: 'tasks#destroy'
